a4edcf9c898ceefb6fedf7feaaddf360184a612a
bazz-the-spazz <44613732+bazz-the-spazz@users.noreply.github.com>
2024-10-30 12:07:33 +0100

    Update function.r

--- function.r
+++ function.r
@@ -3,8 +3,8 @@ formulator <- function(text, words=4, write.file=NULL, sub = NULL, sub.with = NU
 # otionally substitute some parts of the text. "et al." 2 "etal" is standard
 if(is.null(sub)) sub <- character()
 if(is.null(sub.with)) sub.with<- character()
-sub <- c(sub, "et al.")
-sub.with <- c(sub.with, "etal")
+sub <- c(sub, "et al.", "i\\.e\\.", "e\\.g\\.")
+sub.with <- c(sub.with, "etal", "i.e", "e.g")
 if(length(sub)==length(sub.with) | length(sub.with)==1){
 	for( i in 1:length(sub)) text <- gsub(sub[i], sub.with[ifelse(length(sub.with)==1,1,i)], text)
 } else print("sub and sub.with not same length")
@@ -16,6 +16,14 @@ p <- unlist(strsplit(text, "\n"))
 l <- list()
 for(i in seq_along(p)) {
 	x <- unlist(strsplit(p[[i]], "\\. "))
+	
+	# rejoin at digits
+	if(length(x)>0){
+	for(k in length(x):1) if(substr(x[k],nchar(x[k]), nchar(x[k])) %in% 0L:9L ) {
+		x[k] <- paste(x[k] , x[k+1], sep = ". ")
+		x <- x[-(k+1)]
+		}}
+
 	x <- strsplit(x, " ")
 	p2 <- character()
 	for(j in seq_along(x)) p2 <- c(p2, paste(x[[j]][1:ifelse(length(x[[j]])<words, length(x[[j]]), words)], collapse = " "))
